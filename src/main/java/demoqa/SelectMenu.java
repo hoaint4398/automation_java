954bf79357d4286dd43391f82d29f53fc57bf677
DESKTOP-3FRC7CU\FABBI <hoaint@fabbi.com.vn>
2022-12-28 17:01:22 +0700

    28/12

---
+++ src/main/java/demoqa/SelectMenu.java
@@ -0,0 +1,39 @@
+package demoqa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.Objects;
+
+public class SelectMenu {
+    private String url = "https://demoqa.com/select-menu";
+    private WebDriver webDriver;
+
+    @BeforeClass
+    public void beforeClass(){
+        String chromePath = "src/main/resources/browser/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromePath);
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void afterClass(){
+        if(Objects.nonNull(webDriver)) webDriver.quit();
+    }
+
+    public void elementAncestor(){
+        webDriver.get(url);
+        webDriver.findElement(By.xpath("//*[text()='Select Option']//ancestor::div[contains(@class, 'css')]//input"));
+        webDriver.findElement(By.xpath("//*[text()='Select Title']//ancestor::div[contains(@class, 'css')]//input"));
+        webDriver.findElement(By.xpath("//*[text()='Select...']//ancestor::div[contains(@class, 'css')]//input"));
+    }
+
+    public void elementsAncestor(){
+        webDriver.get(url);
+        webDriver.findElements(By.xpath("//*[contains(@class, 'md-6')]//input"));
+    }
+}
